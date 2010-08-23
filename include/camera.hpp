b8c4dd98834b14de0e8ab669a3bb64a2fb634060
sqrt <julius.bullinger+github@gmail.com>
2010-08-23 20:18:35 +0200

    penis

--- include/camera.hpp
+++ include/camera.hpp
@@ -3,26 +3,22 @@
 
 #include "point.hpp"
 
-namespace math3d
+class camera
 {
-	class camera
-	{
-		public:
-			camera(double const&);
-			void calc_dist(double const& width) const;
-			double get_dist() const;
-			
-			double get_pos();
-			
-			~camera();
+	public:
+		camera(double);
+		void calc_dist(double width);
+		double get_dist() const;
+		
+		double get_pos();
+		
+		~camera();
 
-		private:
-			double fovx_;	
-			double distance_;
-			point position_;
+	private:
+		double fovx_;	
+		double distance_;
+		math3d::point position_;
 
-	};
-}
+};
 
-
-#endif //CAMERA_HPP
+#endif // CAMERA_HPP
